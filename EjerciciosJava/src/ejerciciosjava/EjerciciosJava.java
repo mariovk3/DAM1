182c6cddeb36fcd484232607ae59c9e889fb2ff4
Usuario <Usuario@DESKTOP-IJEGIQI.home>
2019-03-15 14:28:35 +0100

    Prueba Git

---
+++ EjerciciosJava/src/ejerciciosjava/EjerciciosJava.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosjava;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EjerciciosJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre = "Mario Alvarez";
+        int edad = 19;
+        System.out.println("Me llamo " + nombre + " y tengo " + edad + " años");
+                
+    }
+    
+}
